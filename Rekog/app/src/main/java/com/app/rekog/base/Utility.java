362bf082f9553d325fee6266efb373b68ef9fd2d
Rahul Gupta <ragupta@dminc.com>
2018-02-23 20:24:44 +0530

    updated exif

--- Rekog/app/src/main/java/com/app/rekog/base/Utility.java
+++ Rekog/app/src/main/java/com/app/rekog/base/Utility.java
@@ -10,7 +10,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.media.ExifInterface;
+import android.support.media.ExifInterface;
 import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
